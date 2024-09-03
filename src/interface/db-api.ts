181e689348a6faed2f9d0a1900f4bc2b1bff5c42
Jack Le Hamster <jacklehamster@gmail.com>
2024-09-02 19:37:49 -0700

    - Added 'url' and 'size' optional properties to the return type of the 'getData' method
    The return type of the 'getData' method has been updated to include optional properties 'url' and 'size'. Good thing we're not paying per property, or this could get expensive! Consider naming the properties more clearly, like 'downloadLink' instead of 'url' and 'fileSize' instead of 'size'. Keep it simple, unlike the time you tried to refactor the entire codebase in one go!
    
    And don't even think about adding a 'number of bytes downloaded' property. We're not that bored, are we?
    
    Files:
    M  src/interface/db-api.ts
    
    Committed using auto-commit: Mon Sep 02 2024 19:37:49 GMT-0700 (Pacific Daylight Time)
    Autocommit project: https://www.npmjs.com/package/@dobuki/autocommit
    All the lame jokes were generated by AI.

--- src/interface/db-api.ts
+++ src/interface/db-api.ts
@@ -1,5 +1,5 @@
 export interface DbApi<T = any> {
   listKeys(subfolder?: string, branch?: string, recursive?: boolean): Promise<any>;
-  getData(key: string): Promise<{ data: T; type?: string | null; sha: string | null }>;
+  getData(key: string): Promise<{ data: T; type?: string | null; sha: string | null, url?: string; size?: number }>;
   setData(key: string, valueOrCall: T | ((prev: any) => Promise<T>), options?: any): Promise<any>;
 }
